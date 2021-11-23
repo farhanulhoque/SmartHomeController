f279a0b04013718ad2a3d87cd2a5e26fa3ba349a
Farhan <farhanulhoque24@gmail.com>
2021-11-22 19:03:02 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,29 +1,12 @@
-/*
- *  Template Developed by Sotirios Liaskos for the needs of
- *   ITEC3030 Systems Architecture.
- *  @ All Rights Reserved
- */
-package itec3030.assignments.a2;
-
-
 import newtemp.NewTempSensor.NewTempSensorDevice;
 import itec3030.smarthome.standards.TemperatureSensor;
 import oldtempinc.hardware.OldSensorDevice;
 
 
-/**
- *
- * @author Sotirios
- */
 public class Main {
     Main p = new Main();
 
     public static void main(String[] args) throws InterruptedException {
-
-        
-        // ***
-        // SCENARIO 1 - Exercise 1
-        // ***
 
         TemperatureSensor o1 = new OldTempSensorAdapter();
         o1.setID("o1");
@@ -35,11 +18,6 @@ public class Main {
         //**** NEW ***** 
         System.out.println("Last reading sent from device was " + o1.getReading());
        
-       
-        
-        // ***
-        // SCENARIO 2 - Exercise 2
-        // ***
         
         //Creating the adapter
         NewTempSensorAdapter o2 = new NewTempSensorAdapter();
@@ -48,11 +26,11 @@ public class Main {
         //Creating a new NewTemp Hardware Device and passing a NewTempSensor in it.
         NewTempSensorDevice newdev = new NewTempSensorDevice(o2.getAdatptee());
         
-        // Part 1: Play the device scenario
+        //Play the device scenario
         newdev.start();
-        // Part 2: Get the last reading
+        //Get the last reading
         System.out.println("Last reading sent from device was " + o2.getReading());
-        // Part 3: Trigger your own temperature event:
+        //Trigger your own temperature event:
         o2.newTemperature(35);
         
         
